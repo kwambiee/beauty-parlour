b229fbb4b1ac1d1279c0b1c4d6797a84ae4615f7
kwambiee <kwambokaj2.jk@gmail.com>
2022-06-19 22:59:46 +0300

    Fix linters

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
+    <div className="App">
+      <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
         <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           Learn React
         </a>
