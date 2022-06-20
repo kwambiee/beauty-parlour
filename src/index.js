a44ad7d26158c748e6e95007b3f2e06e45f8bc98
kwambiee <kwambokaj2.jk@gmail.com>
2022-06-20 07:33:28 +0300

    Add provider to root page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Provider>
       <App />
-    </Router>
+    </Provider>
   </React.StrictMode>
 );
