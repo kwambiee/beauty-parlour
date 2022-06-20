2ac71a1b62bb91a6c64ed619d7717dd7774707c3
kwambiee <kwambokaj2.jk@gmail.com>
2022-06-20 10:46:15 +0300

    Fetch data from API

--- src/Redux/makeupReducer.js
+++ src/Redux/makeupReducer.js
@@ -3,12 +3,20 @@ import axios from "axios";
 const SHOWBRANDS = "react-app/redux/SHOWBRANDS";
 const SHOWPRODUCTS = "react-app/redux/SHOWPRODUCTS";
 
-const showBrands = () => ({
+export const showBrands = () => ({
   type: SHOWBRANDS,
   payload,
 });
 
-const showProducts = () => ({
+export const showProducts = () => ({
   type: SHOWPRODUCTS,
   payload,
 });
+
+export const makeupStore = () => async () => {
+  const response = await axios.get(
+    "http://makeup-api.herokuapp.com/api/v1/products.json"
+  );
+  const data = await response.data;
+  console.log(data);
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 );
