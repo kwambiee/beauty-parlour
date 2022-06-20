54bc2390ea046a6f4a2d55e6f4b69f9b08169528
kwambiee <kwambokaj2.jk@gmail.com>
2022-06-20 10:35:50 +0300

    Create objects

---
+++ src/Redux/makeupReducer.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const SHOWBRANDS = "react-app/redux/SHOWBRANDS";
+const SHOWPRODUCTS = "react-app/redux/SHOWPRODUCTS";
+
+const showBrands = () => ({
+  type: SHOWBRANDS,
+  payload,
+});
+
+const showProducts = () => ({
+  type: SHOWPRODUCTS,
+  payload,
+});
