e4991455528cd1c84d1bdf961d064f56a051591f
kwambiee <kwambokaj2.jk@gmail.com>
2022-06-20 11:00:56 +0300

    Add reducer fro homepage

--- src/Redux/makeupReducer.js
+++ src/Redux/makeupReducer.js
@@ -3,6 +3,8 @@ import axios from "axios";
 const SHOWBRANDS = "react-app/redux/SHOWBRANDS";
 const SHOWPRODUCTS = "react-app/redux/SHOWPRODUCTS";
 
+const initialState = { makeup: [] };
+
 export const showBrands = () => ({
   type: SHOWBRANDS,
   payload,
@@ -13,10 +15,21 @@ export const showProducts = () => ({
   payload,
 });
 
-export const makeupStore = () => async () => {
+export const BrandStore = () => async () => {
   const response = await axios.get(
     "http://makeup-api.herokuapp.com/api/v1/products.json"
   );
   const data = await response.data;
   console.log(data);
 };
+
+const BrandReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SHOWBRANDS:
+      return { makeup: [...action.payload] };
+    default:
+      return state;
+  }
+};
+
+export default BrandReducer;
