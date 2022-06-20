86e175c8f027619f1f43e0e84905ae677f853e46
kwambiee <kwambokaj2.jk@gmail.com>
2022-06-20 18:36:24 +0300

    Adjusted the reducer

--- src/Redux/ConfigureStore.js
+++ src/Redux/ConfigureStore.js
@@ -2,13 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
-import BrandReducer from "./BrandReducer";
+import brandReducer from "./BrandReducer";
+import { fetchProducts } from "./BrandReducer";
 
-const RootReducer = combineReducers({ BrandReducer });
+const RootReducer = combineReducers({ brands: brandReducer });
 
 const Store = configureStore(
   { reducer: RootReducer },
   applyMiddleware(thunk, logger)
 );
+Store.dispatch(fetchProducts());
 
 export default Store;
--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -1,15 +1,10 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { BrandStore } from "../../Redux/BrandReducer";
 
 const HomePage = () => {
-  const dispatch = useDispatch();
   const BrandNames = useSelector((state) => state);
-  console.log(BrandNames);
+  // console.log(BrandNames);
 
-  useEffect(() => {
-    dispatch(BrandStore());
-  }, []);
   return <div>HomePage</div>;
 };
 export default HomePage;
