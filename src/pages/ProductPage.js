c0969a247f8131526ae4991c64f03ae44320ec1c
kwambiee <kwambokaj2.jk@gmail.com>
2022-06-22 16:10:17 +0300

    Fix linters

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,7 +1,7 @@
 /* eslint eqeqeq: 0 */
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function Productpage() {
   const [product, setProduct] = useState({});
@@ -12,7 +12,7 @@ export default function Productpage() {
   useEffect(() => {
     const getProduct = () => {
       const newState = products.find(
-        (product) => product.id == params.productId
+        (product) => product.id == params.productId,
       );
 
       setProduct(newState || {});
@@ -24,51 +24,45 @@ export default function Productpage() {
     <div>
       <div
         style={{
-          display: "flex",
-          margin: "0 15%",
+          display: 'flex',
+          margin: '0 15%',
         }}
       >
         <div>
           <img
             src={product.image_link}
-            alt='makeup product'
+            alt="makeup product"
             onError={(e) => {
               e.target.onerror = null;
-              e.target.src =
-                "https://images.pexels.com/photos/3018845/pexels-photo-3018845.jpeg";
+              e.target.src = 'https://images.pexels.com/photos/3018845/pexels-photo-3018845.jpeg';
             }}
             style={{
-              height: "300px",
-              borderRadius: "5px",
-              marginRight: "2rem",
+              height: '300px',
+              borderRadius: '5px',
+              marginRight: '2rem',
             }}
           />
           <p
             style={{
-              margin: "1rem 0 0 4rem",
-              backgroundColor: "peachpuff",
-              border: "2px solid transparent",
-              borderRadius: "5px",
-              width: "100px",
+              margin: '1rem 0 0 4rem',
+              backgroundColor: 'peachpuff',
+              border: '2px solid transparent',
+              borderRadius: '5px',
+              width: '100px',
             }}
           >
             {product.price_sign}
             {product.price}
           </p>
-          <div>
-            {product.tag_list.map((tag) => (
-              <span>{tag}</span>
-            ))}
-          </div>
         </div>
-        <div style={{ border: "1px solid #FEC2D6" }}>
+        <div style={{ border: '1px solid #FEC2D6' }}>
           <p
             style={{
-              width: "500px",
-              backgroundColor: "#FEC2D6",
-              padding: "3rem",
-              margin: "0.5rem",
-              borderRadius: "5px",
+              width: '500px',
+              backgroundColor: '#FEC2D6',
+              padding: '3rem',
+              margin: '0.5rem',
+              borderRadius: '5px',
             }}
           >
             {product.description}
