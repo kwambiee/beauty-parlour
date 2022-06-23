8ba288981de7b8c1a53cbce1b75ecc8e4b56554e
kwambiee <kwambokaj2.jk@gmail.com>
2022-06-23 11:23:07 +0300

    Add back button

--- src/pages/BrandPage.js
+++ src/pages/BrandPage.js
@@ -1,22 +1,35 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import Product from '../components/product';
+import React from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Product from "../components/product";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 const BrandPage = () => {
   const params = useParams();
   const brandNames = useSelector((state) => state.brands.brands);
   const products = brandNames[params.brandName] || [];
 
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/");
+  };
+
   return (
-    <div
-      className="products"
-      style={{ backgroundImage: 'url(/assets/makeup.jpg)' }}
-    >
-      <p style={{ display: 'none' }}>{params.brandName}</p>
-      {products.map((product) => (
-        <Product details={product} id={product.id} key={product.id} />
-      ))}
+    <div>
+      <div>
+        <button onClick={handleClick}>
+          <ArrowBackIcon />
+        </button>
+      </div>
+      <div
+        className='products'
+        style={{ backgroundImage: "url(/assets/makeup.jpg)" }}
+      >
+        <p style={{ display: "none" }}>{params.brandName}</p>
+        {products.map((product) => (
+          <Product details={product} id={product.id} key={product.id} />
+        ))}
+      </div>
     </div>
   );
 };
--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,7 +1,8 @@
 /* eslint eqeqeq: 0 */
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 export default function Productpage() {
   const [product, setProduct] = useState({});
@@ -12,7 +13,7 @@ export default function Productpage() {
   useEffect(() => {
     const getProduct = () => {
       const newState = products.find(
-        (product) => product.id == params.productId,
+        (product) => product.id == params.productId
       );
 
       setProduct(newState || {});
@@ -20,54 +21,61 @@ export default function Productpage() {
     getProduct();
   }, []);
 
+  const navigate = useNavigate();
+
   return (
     <div>
+      <div>
+        <button onClick={() => navigate(-1)}>
+          <ArrowBackIcon />
+        </button>
+      </div>
       <div
-        className="productPage"
+        className='productPage'
         style={{
-          display: 'flex',
-          backgroundImage: 'url(/assets/makeup.jpg)',
-          padding: '2rem',
+          display: "flex",
+          backgroundImage: "url(/assets/makeup.jpg)",
+          padding: "2rem",
         }}
       >
-        <div className="productDetails">
+        <div className='productDetails'>
           <img
             src={product.image_link}
-            alt="makeup product"
+            alt='makeup product'
             onError={(e) => {
               e.target.onerror = null;
-              e.target.src = 'https://images.pexels.com/photos/3018845/pexels-photo-3018845.jpeg';
+              e.target.src =
+                "https://images.pexels.com/photos/3018845/pexels-photo-3018845.jpeg";
             }}
             style={{
-              height: '300px',
-              borderRadius: '5px',
-              marginRight: '2rem',
+              height: "300px",
+              borderRadius: "5px",
+              marginRight: "2rem",
             }}
           />
           <p
-            className="price"
+            className='price'
             style={{
-              margin: '1rem 0 0 4rem',
-              backgroundColor: 'peachpuff',
-              border: '2px solid transparent',
-              borderRadius: '5px',
-              width: '100px',
+              margin: "1rem 0 0 4rem",
+              backgroundColor: "peachpuff",
+              border: "2px solid transparent",
+              borderRadius: "5px",
+              width: "100px",
             }}
           >
             {product.price_sign}
             {product.price}
           </p>
         </div>
-        <div style={{ border: '1px solid #FEC2D6' }} className="productBorder">
+        <div style={{ border: "1px solid #FEC2D6" }} className='productBorder'>
           <p
             style={{
-              // width: "500px",
-              backgroundColor: '#FEC2D6',
-              padding: '3rem',
-              margin: '0.5rem',
-              borderRadius: '5px',
+              backgroundColor: "#FEC2D6",
+              padding: "3rem",
+              margin: "0.5rem",
+              borderRadius: "5px",
             }}
-            className="productDescription"
+            className='productDescription'
           >
             {product.description}
           </p>
